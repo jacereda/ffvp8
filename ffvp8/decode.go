23b69f3bebd0adb6d18f45a2954fbd98bf008c1e
Jorge Acereda <jacereda@gmail.com>
2012-10-01 01:55:49 +0200

    Free frames pool

--- ffvp8/decode.go
+++ ffvp8/decode.go
@@ -9,6 +9,7 @@ package ffvp8
 // #cgo LDFLAGS: -lavcodec
 //
 // #include "libavcodec/avcodec.h"
+// #include <string.h>
 // extern AVCodec ff_vp8_decoder;
 // static int get_buffer(AVCodecContext * cc, AVFrame * f) { 
 //   void vp8GetBuffer(AVCodecContext * cc, AVFrame * f);
@@ -23,6 +24,7 @@ package ffvp8
 // }
 // static void install_callbacks(AVCodecContext * cc) {
 //   cc->get_buffer = get_buffer;
+//   cc->reget_buffer = 0;
 //   cc->release_buffer = release_buffer;
 // }
 import "C"
@@ -38,6 +40,11 @@ import (
 type Frame struct {
 	*image.YCbCr
 	Timecode time.Duration
+	dec      *Decoder
+}
+
+func (f *Frame) Release() {
+	f.dec.free <- f
 }
 
 func init() {
@@ -47,7 +54,8 @@ func init() {
 type Decoder struct {
 	c    *C.AVCodec
 	cc   *C.AVCodecContext
-	imgs list.List
+	busy list.List
+	free chan *Frame
 }
 
 //export vp8GetBuffer
@@ -68,44 +76,56 @@ func aligned(x int) int {
 	return (x+15)&-16 + 16
 }
 
-func (d *Decoder) getBuffer(cc *C.AVCodecContext, fr *C.AVFrame) {
-	w := int(cc.width)
-	h := int(cc.height)
-	aw := aligned(w)
-	ah := aligned(h)
-	acw := aligned(w / 2)
-	ach := aligned(h / 2)
-	ysz := aw * ah
-	csz := acw * ach
-	b := make([]byte, ysz+2*csz)
-	img := &image.YCbCr{
-		Y:              b[:ysz],
-		Cb:             b[ysz : ysz+csz],
-		Cr:             b[ysz+csz : ysz+2*csz],
-		SubsampleRatio: image.YCbCrSubsampleRatio420,
-		YStride:        aw,
-		CStride:        acw,
-		Rect:           image.Rect(0, 0, w, h),
+func (d *Decoder) allocFrame() *Frame {
+	var frame *Frame
+	select {
+	case frame = <-d.free:
+	default:
+		w := int(d.cc.width)
+		h := int(d.cc.height)
+		aw := aligned(w)
+		ah := aligned(h)
+		acw := aligned(w / 2)
+		ach := aligned(h / 2)
+		ysz := aw * ah
+		csz := acw * ach
+		b := make([]byte, ysz+2*csz)
+		frame = &Frame{
+			&image.YCbCr{
+				Y:              b[:ysz],
+				Cb:             b[ysz : ysz+csz],
+				Cr:             b[ysz+csz : ysz+2*csz],
+				SubsampleRatio: image.YCbCrSubsampleRatio420,
+				YStride:        aw,
+				CStride:        acw,
+				Rect:           image.Rect(0, 0, w, h),
+			}, 0, d}
 	}
-	e := d.imgs.PushBack(img)
+	return frame
+}
+
+func (d *Decoder) getBuffer(cc *C.AVCodecContext, fr *C.AVFrame) {
+	frame := d.allocFrame()
+	fr.opaque = unsafe.Pointer(d.busy.PushBack(frame))
+	img := frame.YCbCr
 	fr.data[0] = (*C.uint8_t)(&img.Y[0])
 	fr.data[1] = (*C.uint8_t)(&img.Cb[0])
 	fr.data[2] = (*C.uint8_t)(&img.Cr[0])
 	fr.linesize[0] = C.int(img.YStride)
 	fr.linesize[1] = C.int(img.CStride)
 	fr.linesize[2] = C.int(img.CStride)
-	fr.width = C.int(w)
-	fr.height = C.int(h)
+	fr.width = C.int(cc.width)
+	fr.height = C.int(cc.height)
 	fr.format = C.int(cc.pix_fmt)
 	fr.sample_aspect_ratio = cc.sample_aspect_ratio
 	fr.pkt_pts = C.AV_NOPTS_VALUE
-	fr.opaque = unsafe.Pointer(e)
 }
 
 func (d *Decoder) releaseBuffer(cc *C.AVCodecContext, fr *C.AVFrame) {
-	var e *list.Element
-	e = (*list.Element)(fr.opaque)
-	d.imgs.Remove(e)
+	fr.data[0] = nil
+	fr.data[1] = nil
+	fr.data[2] = nil
+	d.busy.Remove((*list.Element)(fr.opaque))
 }
 
 func NewDecoder() *Decoder {
@@ -115,6 +135,7 @@ func NewDecoder() *Decoder {
 	d.cc.opaque = unsafe.Pointer(&d)
 	C.install_callbacks(d.cc)
 	C.avcodec_open2(d.cc, d.c, nil)
+	d.free = make(chan *Frame, 8)
 	return &d
 }
 
@@ -129,5 +150,7 @@ func (d *Decoder) Decode(data []byte, tc time.Duration) *Frame {
 	if C.avcodec_decode_video2(d.cc, &fr, &got, &pkt) < 0 || got == 0 {
 		log.Panic("Unable to decode")
 	}
-	return &Frame{(*list.Element)(fr.opaque).Value.(*image.YCbCr), tc}
+	frame := (*list.Element)(fr.opaque).Value.(*Frame)
+	frame.Timecode = tc
+	return frame
 }
